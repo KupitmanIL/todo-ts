cbaa8eb7d89a1f4e5f4d6083b114d399e9f57413
edward <edwardlfish@gmail.com>
2026-03-02 21:07:55 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import Task from "./components/Task.tsx";
+
+function App() {
+    const [tasks, setTasks] = useState<string[]>([]);
+
+    const deleteTask = (index: number) => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    }
+
+    const editTask = (index: number, text: string) => {
+        const newTasks = [...tasks];
+        newTasks[index] = text;
+        setTasks(newTasks);
+    }
+
+    const addTask = () => {
+        setTasks([...tasks, 'New task']);
+    }
+
+    return (
+        <div className={'field'}>
+            <button className={'btn new'} onClick={addTask}>Add task</button>
+            {tasks.map((t, i) => <Task key={i + 1} index={i} edit={editTask} remove={deleteTask}>{t}</Task>)}
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Task.tsx
@@ -0,0 +1,45 @@
+import {useRef, useState} from "react";
+
+interface Props {
+    children: string,
+    index: number,
+    remove: (index: number) => void,
+    edit: (index: number, text: string) => void
+}
+
+const Task = ({children, index, remove, edit}: Props) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const textId = useRef<HTMLTextAreaElement>(null);
+
+    const handleClickEdit = () => {
+        setIsEditing(true);
+    }
+
+    const handleClickRemove = () => {
+        remove(index);
+    }
+
+    const handleClickSave = () => {
+        edit(index, textId.current!.value);
+        setIsEditing(false);
+    }
+
+    if (isEditing) {
+        return (
+            <div className={'box'}>
+                <textarea ref={textId} defaultValue={children}></textarea>
+                <button onClick={handleClickSave} className={'btn success'}>Save</button>
+            </div>
+        )
+    } else {
+        return (
+            <div className={'box'}>
+                <div>{children}</div>
+                <button onClick={handleClickEdit} className={'btn light'}>Edit</button>
+                <button onClick={handleClickRemove} className={'btn red'}>Remove</button>
+            </div>
+        )
+    }
+}
+
+export default Task;
